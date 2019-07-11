eb3ea1eb4b338de0ca335d7856762a0f9169090d
abhishek-31 <abhishek31ag@gmail.com>
2019-07-11 20:27:45 +0530

    Added script

---
+++ Population.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 20:01:06 2019
+
+@author: abhishek
+"""
+import re
+with open("/home/abhishek/Documents/FAQ-Builder/Delhi.txt","r+") as f:
+    a=f.read()
+    b=a.split(".")
+#    print(len(b))
+#    print(b)
+    for _ in b:
+        if "population" in _:
+            while ',' in _:
+                _=_.replace(',',"") #For removing commas from between the population figures.
+            print(_)
+            array = re.findall(r'[0-9]+', _)
+            for k in array:
+                if(_.find("population")<_.find(k)):  #Sometimes year figure is given before population word and most probably the number coming after population keyword is desired output.
+                    print(k)
+                    break
+            break

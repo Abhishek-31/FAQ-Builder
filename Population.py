cffb9baeedbe2c256e4e7caebc6a19c2beacc6ed
abhishek-31 <abhishek31ag@gmail.com>
2019-07-12 14:43:27 +0530

    Added population code

--- Population.py
+++ Population.py
@@ -9,19 +9,31 @@ Created on Thu Jul 11 20:01:06 2019
 
 
 import re
-with open("/home/abhishek/Documents/FAQ-Builder/Delhi.txt","r+") as f:
-    a=f.read()
-    b=a.split(".")
-#    print(len(b))
-#    print(b)
-    for _ in b:
-        if "population" in _:
-            while ',' in _:
-                _=_.replace(',',"") #For removing commas from between the population figures.
-            print(_)
-            array = re.findall(r'[0-9]+', _)
-            for k in array:
-                if(_.find("population")<_.find(k)):  #Sometimes year figure is given before population word and most probably the number coming after population keyword is desired output.
-                    print(k)
-                    break
-            break
+city=["/home/abhishek/Documents/FAQ-Builder/Delhi.txt","/home/abhishek/Documents/FAQ-Builder/East York.txt",'/home/abhishek/Documents/FAQ-Builder/Yatomi.txt']
+for each in city:
+    with open(each,"r+") as f:
+        a=f.read()
+        b=a.split(".")
+    
+    #    print(len(b))
+    #    print(b)
+        for _ in b:
+            if "population" in _:
+                while ' billion' in _:
+                    _=_.replace(' billion',"000000000")
+                while ' million' in _:
+                    _=_.replace(' million',"000000")
+                while ' thousand' in _:
+                    _=_.replace(' thousand',"000")
+                while ',' in _:
+                    _=_.replace(',',"") #For removing commas from between the population figures.
+                print(_)
+                array = re.findall(r'[0-9]+', _)
+#                print(array)
+#                d=[_.find(k) for k in array]
+#                print(d)
+                for k in array:
+                    if(_.find("population")<_.find(" "+k)):  #Sometimes year figure is given before population word and most probably the number coming after population keyword is desired output.
+                        print(k)     # space is added because the same number can be [resent in anmy figure in the sentence and hemnce could create confusion.
+                        break
+                break
